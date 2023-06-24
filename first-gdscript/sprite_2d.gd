a7e3b02814b4a584d93089b5b7b3fd58b97801d0
dahliakyu <lamiyavi@hotmail.com>
2023-06-24 18:06:31 +0200

    first-commit

---
+++ first-gdscript/sprite_2d.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+var speed = 60
+var angular_speed = PI
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("rot_anticlockwise"):
+		direction = -1
+	if Input.is_action_pressed("rot_clockwise"):
+		direction = 1
+	
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2.DOWN.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_left"):
+		velocity = Vector2.LEFT.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_right"):
+		velocity = Vector2.RIGHT.rotated(rotation) * speed
+	
+	position += velocity * delta
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
